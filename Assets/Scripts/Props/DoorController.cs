0911d2722de8b511ccef43b3b3f89a4047aab913
Amit Ethan Halpert <amit.ethan.halpert@gmail.com>
2022-08-23 15:58:17 +0300

    Added A DoorController & DoorRaycast

---
+++ Assets/Scripts/Props/DoorController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DoorController : MonoBehaviour
+{
+
+
+    private Animator doorAnim;
+    private bool doorOpen = false;
+
+
+    private void Awake()
+    {
+        doorAnim = gameObject.GetComponent<Animator>();
+    }
+
+    public void PlayAnimation()
+    {
+        if (!doorOpen)
+        {
+            SceneManager.LoadScene("TheRoad", LoadSceneMode.Single);
+            doorOpen = true;
+        }
+        else
+        {
+            doorOpen = false;
+        }
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
